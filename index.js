298384ef00f7bd72d466b94390cdf0c83271e0f5
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-23 16:50:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+let createEmployeeRecord = function(row) {
+  return {
+    firstName: row[0],
+    familyName: row[1],
+    title: row[2],
+    payPerHour: row[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+//this is creating multiple records
+let createEmployeeRecords = function(employeeRowData) {
+  return employeeRowData.map( function(row){
+    return createEmployeeRecord(row)
+  })
+}
+
+let createTimeInEvent = function(employee, dateStamp) {
+  let [date, hour] = dateStamp.split(' ')
+
+  employee.timeInEvents.push({
+    type: 'TimeIn',
+    hour: parseInt(hour, 10),
+    date,
+  })
+
+  return employee
+}
+
+let createTimeOutEvent = function(employee, dateStamp) {
+  let [date, hour] = dateStamp.split(' ')
+
+  employee.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  })
+
+  return employee
+}
+
+let hoursWorkedOnDate = function(employee, soughtDate) {
+
+  let clockIn = employee.timeInEvents.find(function(e){
+    return e.date === soughtDate
+  })
+
+  let clockOut = employee.timeOutEvents.find(function(e){
+    return e.date === soughtDate
+  })
+
+  return (clockOut.hour - clockIn.hour) / 100
+}
